82814f7be3764608040b34ba80730b1c72d83908
JonyDuarte <joaoc84duarte@gmail.com>
2024-09-23 19:59:38 -0300

    versão 1.0

---
+++ app.py
@@ -0,0 +1,7 @@
+# entrada de dados
+nome = input("Informe seu nome: ")
+idade = int(input("Informe sua idade: "))
+
+# verifica a idade do usuário
+print(f"{nome} é maior de idade." if idade >= 18 else f" {nome} é menor de idade.")
+
